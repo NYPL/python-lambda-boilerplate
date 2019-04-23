ee1a13ac0dfcb118b59edf48ed105b81c9d0c0f9
Mike Benowitz <mwbenowitz@gmail.com>
2019-04-23 10:23:43 -0400

    Refactor `lambdaRun` for better maintainability
    This refactors the `regex` based structure of the file that
    managed the specific type of invocation being made to run the
    lambda function.
    
    This is replaced with an explicit mapping between the valid
    execution commands and the methods that they invoke. In the case
    that a command is received that is not found and error method
    is called, which raises an `InvalidExectutionType` error.
    
    This extracts a large amount of functionality from a single method
    into several smaller methods, allowing for easier tracing of issues
    and clearer visualisation of the path through this module.

--- scripts/lambdaRun.py
+++ scripts/lambdaRun.py
@@ -1,7 +1,6 @@
 import subprocess
 import sys
 import os
-import re
 
 from helpers.logHelpers import createLog
 from helpers.errorHelpers import InvalidExecutionType
@@ -13,63 +12,111 @@ logger = createLog('runScripts')
 
 def main():
     """Invoked by the makefile's arguments, controls the overall execution of
-    the Lambda function. h/t to nonword for inspiration to use a makefile.
-
-    Raises:
-        InvalidExecutionType: If the args do not contain a valid execution type
-        raise an error.
-    """
+    the Lambda function. h/t to nonword for inspiration to use a makefile."""
+    print(sys.argv)
     if len(sys.argv) != 2:
         logger.warning('This script takes one, and only one, argument!')
         sys.exit(1)
+
     runType = sys.argv[1]
 
-    if re.match(r'^(?:local|development|qa|production)', runType):
-        logger.info('Deploying lambda to {} environment'.format(runType))
-        setEnvVars(runType)
-        subprocess.run([
-            'lambda',
-            'deploy',
-            '--config-file',
-            'run_config.yaml',
-            '--requirements',
-            'requirements.txt'
-        ])
-        os.remove('run_config.yaml')
-        createEventMapping(runType)
+    # Map the possible valid execution commands.
+    runTypeFuncs = {
+        'local': deployFunc,
+        'development': deployFunc,
+        'qa': deployFunc,
+        'production': deployFunc,
+        'run-local': runFunc,
+        'build-development': buildFunc,
+        'build-qa': buildFunc,
+        'build-production': buildFunc
+    }
 
-    elif re.match(r'^run-local', runType):
-        logger.info('Running test locally with development environment')
-        env = 'local'
-        setEnvVars(env)
-        subprocess.run([
-            'lambda',
-            'invoke',
-            '-v',
-            '--config-file',
-            'run_config.yaml'
-        ])
-        os.remove('run_config.yaml')
+    # Execute desired function. If not found, raise an error.
+    runTypeFuncs.get(runType, errFunc)(runType)
 
-    elif re.match(r'^build-(?:development|qa|production)', runType):
-        env = runType.replace('build-', '')
-        logger.info(
-            'Building package for {} environment, will be in dist/'.format(env)
-        )
-        setEnvVars(env)
-        subprocess.run([
-            'lambda',
-            'build',
-            '--requirements',
-            'requirements.txt',
-            '--config-file',
-            'run_config.yaml'
-        ])
-        os.remove('run_config.yaml')
+    # Remove computed configuration file. This is only used during runtime.
+    os.remove('run_config.yaml')
 
-    else:
-        logger.error('Execution type not recognized! {}'.format(runType))
-        raise InvalidExecutionType('{} is not a valid command'.format(runType))
+
+def deployFunc(runType):
+    """Deploys the current Lambda function to the environment specificied
+    in the current configuration file.
+
+    Arguments:
+        runType {string} -- The environment to deploy the function to. Should
+        be one of [local|development|qa|production]
+    """
+    logger.info('Deploying lambda to {} environment'.format(runType))
+    runProcess(runType, [
+        'deploy',
+        '--config-file',
+        'run_config.yaml',
+        '--requirements',
+        'requirements.txt'
+    ])
+    createEventMapping(runType)
+
+
+def buildFunc(runType):
+    """Builds a deployment package that can be uploaded to AWS or a testing
+    environment.
+
+    Arguments:
+        runType {string} -- The environment to build the function for. Should
+        be one of [development|qa|production]. (Local builds are unnecessary as
+        the code can be executed from the development directory.)
+    """
+    buildEnv = runType.replace('build-', '')
+    logger.info(
+        'Building package for {}, will be in dist/'.format(buildEnv)
+    )
+    runProcess(buildEnv, [
+        'build',
+        '--requirements',
+        'requirements.txt',
+        '--config-file',
+        'run_config.yaml'
+    ])
+
+
+def runFunc(runType):
+    """Invokes the lambda function with currently configured local settings.
+
+    Arguments:
+        runType {string} -- The environment variables to execute the function
+        with, should always be local.
+    """
+    logger.info('Running test locally with development environment')
+    runProcess('local', ['invoke', '-v', '--config-file', 'run_config.yaml'])
+
+
+def errFunc(runType):
+    """Raises an error when an unknown command is received.
+
+    Arguments:
+        runType {string} -- The unknown command received by this function,
+
+    Raises:
+        InvalidExecutionType: Indicates that this module cannot execute the
+        requested command.
+    """
+    logger.error('Execution type not recognized! {}'.format(runType))
+    raise InvalidExecutionType('{} is not a valid command'.format(runType))
+
+
+def runProcess(runType, argList):
+    """Executes the requested command through a subprocess call to the CLI.
+
+    Arguments:
+        runType {string} -- The current environment to load settings for.
+        argList {array} -- An array of command line arguments that dictate
+        the specific type of invocation being made to `python-lambda`
+    """
+    setEnvVars(runType) # Creates the run_config.yaml file
+    processArgs = ['lambda']
+    processArgs.extend(argList)
+    subprocess.run(processArgs) # Executes the actual command
 
 
 if __name__ == '__main__':
--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -3,7 +3,14 @@ from unittest.mock import patch
 import logging
 import sys
 
-from scripts.lambdaRun import main
+from scripts.lambdaRun import (
+    main,
+    deployFunc,
+    buildFunc,
+    runFunc,
+    errFunc,
+    runProcess
+)
 from helpers.errorHelpers import InvalidExecutionType
 
 # Disable logging while we are running tests
@@ -13,44 +20,12 @@ logging.disable(logging.CRITICAL)
 class TestScripts(unittest.TestCase):
 
     @patch.object(sys, 'argv', ['make', 'development'])
-    @patch('scripts.lambdaRun.setEnvVars')
-    @patch('scripts.lambdaRun.createEventMapping')
-    @patch('subprocess.run')
+    @patch('scripts.lambdaRun.deployFunc')
     @patch('os.remove')
-    def test_run_deploy(self, mock_rm, mock_run, mock_env, mock_envVars):
+    def test_run_main(self, mock_remove, mock_deploy):
         main()
-        mock_envVars.assert_called_once()
-        mock_env.assert_called_once()
-        mock_run.assert_called_once()
-        mock_rm.assert_called_once()
-
-    @patch.object(sys, 'argv', ['make', 'run-local'])
-    @patch('scripts.lambdaRun.setEnvVars')
-    @patch('subprocess.run')
-    @patch('os.remove')
-    def test_run_local(self, mock_rm, mock_run, mock_envVars):
-        main()
-        mock_envVars.assert_called_once_with('local')
-        mock_run.assert_called_once()
-        mock_rm.assert_called_once_with('run_config.yaml')
-
-    @patch.object(sys, 'argv', ['make', 'build-development'])
-    @patch('scripts.lambdaRun.setEnvVars')
-    @patch('subprocess.run')
-    @patch('os.remove')
-    def test_run_build(self, mock_rm, mock_run, mock_envVars):
-        main()
-        mock_envVars.assert_called_once_with('development')
-        mock_run.assert_called_once()
-        mock_rm.assert_called_once_with('run_config.yaml')
-
-    @patch.object(sys, 'argv', ['make', 'bad-command'])
-    def test_run_invalid(self):
-        try:
-            main()
-        except InvalidExecutionType:
-            pass
-        self.assertRaises(InvalidExecutionType)
+        mock_deploy.assert_called_once_with('development')
+        mock_remove.assert_called_once_with('run_config.yaml')
 
     @patch.object(sys, 'argv', ['make', 'hello', 'jerry'])
     def test_run_surplus_argv(self):
@@ -59,6 +34,59 @@ class TestScripts(unittest.TestCase):
         except SystemExit:
             pass
         self.assertRaises(SystemExit)
+
+    @patch.object(sys, 'argv', ['make', 'bad_function'])
+    @patch('scripts.lambdaRun.errFunc')
+    @patch('os.remove')
+    def test_run_bad_function(self, mock_remove, mock_err):
+        main()
+        mock_err.assert_called_once_with('bad_function')
+        mock_remove.assert_called_once_with('run_config.yaml')
+
+    @patch('scripts.lambdaRun.runProcess')
+    @patch('scripts.lambdaRun.createEventMapping')
+    def test_deploy_function(self, mock_create, mock_run):
+        deployFunc('test')
+        mock_run.assert_called_once()
+        mock_create.assert_called_once_with('test')
+
+    @patch('scripts.lambdaRun.runProcess')
+    def test_build_function(self, mock_run):
+        buildFunc('test')
+        mock_run.assert_called_once_with(
+            'test',
+            [
+                'build',
+                '--requirements',
+                'requirements.txt',
+                '--config-file',
+                'run_config.yaml'
+            ]
+        )
+
+    @patch('scripts.lambdaRun.runProcess')
+    def test_run_function(self, mock_run):
+        runFunc('test')
+        mock_run.assert_called_once_with(
+            'local', 
+            ['invoke', '-v', '--config-file', 'run_config.yaml']
+        )
+
+    def test_err_function(self):
+        try:
+            errFunc('bad_command')
+        except InvalidExecutionType:
+            pass
+        self.assertRaises(InvalidExecutionType)
+
+    @patch('scripts.lambdaRun.setEnvVars')
+    @patch('subprocess.run')
+    def test_run_process(self, mock_run, mock_envVars):
+        runProcess('test', ['run', 'test'])
+        mock_envVars.assert_called_once_with('test')
+        mock_run.assert_called_once_with(
+            ['lambda', 'run', 'test']
+        )
 
 
 if __name__ == '__main__':
